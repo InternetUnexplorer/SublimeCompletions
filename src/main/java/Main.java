dc2e649085f542fb737a2b88d484b30176b3f417
InternetUnexplorer <ferncactus@gmail.com>
2016-06-18 20:47:52 -0600

    More basic UI!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,12 @@
  */
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -12,7 +17,14 @@ public class Main extends Application {
 	}
 
 	@Override
-	public void start(Stage primaryStage) {
-
+	public void start(Stage primaryStage) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/Main.fxml"));
+		BorderPane parent = loader.load();
+		parent.getStylesheets().add(getClass().getResource("/dark.css").toExternalForm());
+		primaryStage.setScene(
+				new Scene(parent, 600, 400)
+		);
+		primaryStage.setTitle("Sublime Completions Editor");
+		primaryStage.show();
 	}
 }
