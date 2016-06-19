bda6ee6551c8fddb461e72e3b507c98750ba2bc6
InternetUnexplorer <ferncactus@gmail.com>
2016-06-18 20:43:03 -0600

    Added some basic UI

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Alex on 6/18/2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+
+	}
+}
